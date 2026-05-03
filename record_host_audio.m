d0aef4059a656d03e8be31db37f1fa03e394d8da
NWalker4483 <nile.walker@jhuapl.edu>
2026-05-03 12:49:21 -0400

    ReOrg

--- record_host_audio.m
+++
@@ -1,9 +0,0 @@
-function signal1 = record_host_audio(audio_length,fs)
-    recObj1 = audiorecorder(fs,16,1,1);
-
-    record(recObj1);
-    pause(audio_length);
-    stop(recObj1);
-    
-    signal1 = getaudiodata(recObj1);
-end
